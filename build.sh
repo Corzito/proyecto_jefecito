cef9531f6e1cc157afb0f69f63070f88aac08174
admin <corzo3032@gmail.com>
2026-05-13 20:44:28 -0500

    feat: crear superusuario automatico en deploy

--- build.sh
+++ build.sh
@@ -3,3 +3,8 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'corzo3032@gmail.com', 'Admin2026!')
+"
