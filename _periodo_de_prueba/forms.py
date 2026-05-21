c295cc437143efb10ff96105b4316b1c47841bd0
admin <corzo3032@gmail.com>
2026-05-21 15:27:40 -0500

    refactor: quitar correo jefe y enlace jefes del navbar

--- _periodo_de_prueba/forms.py
+++ _periodo_de_prueba/forms.py
@@ -7,14 +7,13 @@ class ColaboradorForm(forms.ModelForm):
         model = Colaborador
         fields = [
             'cedula', 'nombres', 'cargo', 'jefe_inmediato',
-            'correo_jefe', 'empresa', 'celular', 'fecha_ingreso'
+            'empresa', 'celular', 'fecha_ingreso'
         ]
         widgets = {
             'cedula': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ej: 1055247375'}),
             'nombres': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre completo'}),
             'cargo': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Cargo'}),
             'jefe_inmediato': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre del jefe'}),
-            'correo_jefe': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'correo@empresa.com'}),
             'empresa': forms.Select(attrs={'class': 'form-control'}),
             'celular': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'No. Celular'}),
             'fecha_ingreso': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
